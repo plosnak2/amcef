56535e1b76e3c515ee5a9e1de6dc391662d897cb
plosnak2 <jakub.zaukolec@gmail.com>
2023-04-11 14:12:10 +0200

    fit logo added

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -10,7 +10,7 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import InputBase from '@mui/material/InputBase';
 import SearchIcon from '@mui/icons-material/Search';
-import logo from '../images/amcef_logo_final.png'
+import logo from '../images/FIT.png'
 import { Link } from "react-router-dom";
 import { useLocation } from 'react-router-dom'
 
